fc1f8db52f10fe673e628d02b953e1291900221b
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2018-10-25 10:22:25 +0300

    Updated CounterHashMap

--- CounterHashMap.h
+++ CounterHashMap.h
@@ -9,9 +9,19 @@
 using namespace std;
 
 template <class K> class CounterHashMap : public map<K, int> {
+private:
+    bool containsKey(K key);
 public:
     CounterHashMap();
     void put(K key);
+    void putNTimes(K key, int N);
+    int count(K key);
+    int sumOfCounts();
+    K max();
+    K max(double threshold);
+    void add(CounterHashMap<K> toBeAdded);
+    vector<pair<K, int>> topN(int N);
+    string to_string();
 };
 
 
