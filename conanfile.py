8997cb0493ff130af5e58ce68a13cf6671a04faa
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-08-21 20:52:06 +0300

    Updated Conan for header only packaging.

--- conanfile.py
+++ conanfile.py
@@ -9,28 +9,11 @@ class DataStructureConan(ConanFile):
     url = "https://github.com/StarlangSoftware/DataStructure-CPP"
     description = "DataStructure Library"
     topics = ("")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    generators = "cmake"
-    exports_sources = "src/*", "Test/*"
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder="src")
-        cmake.build()
+    exports_sources = "src/*"
+    no_copy_source = True
 
     def package(self):
         self.copy("*.h", dst="include", keep_path=False)
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["DataStructure"]
