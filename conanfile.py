9dcad1d9e536ebfc554c9f357422c2a43ad26dd7
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-08-21 18:35:48 +0300

    Added Conan for packaging.

--- conanfile.py
+++ conanfile.py
@@ -25,7 +25,7 @@ class DataStructureConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="")
+        self.copy("*.h", dst="include", keep_path=False)
         self.copy("*hello.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
