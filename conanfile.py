9d87b6b1f2d8885be3bfff1f1a46ca51921b8840
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-03-09 11:52:18 +0300

    Updated conanfile.py for Conan 2.1.0.

--- conanfile.py
+++ conanfile.py
@@ -16,4 +16,4 @@ class DataStructureConan(ConanFile):
     no_copy_source = True
 
     def package(self):
-        copy("*.h", keep_path=False, src=join(self.source_folder), dst=join(self.package_folder, "include"), pattern="*.h")
+        copy(conanfile=self, keep_path=False, src=join(self.source_folder), dst=join(self.package_folder, "include"), pattern="*.h")
